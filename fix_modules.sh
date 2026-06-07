bf2967e05799d7618a2a71b4b456736ef060eef2
jaoe1000 <jaoe1000@aol.com>
2026-06-07 08:49:11 -0400

    Update fix_modules.sh

--- fix_modules.sh
+++ fix_modules.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-# Flatten nested modules: if /modules/dwellings/dwellings exists, move contents up
+# fix_modules.sh - Targeted at NB-Core's dual-structure
+
 for dir in /tmp/modules/*; do
     if [ -d "$dir" ]; then
         base=$(basename "$dir")
+        
+        # Check if this is one of the "double-nested" NB-Core modules
+        # It looks for a folder inside that matches the parent name
         if [ -d "$dir/$base" ]; then
+            # Move everything from the inner folder up to the module root
             mv "$dir/$base"/* "$dir/"
+            # Remove the now-empty inner folder
             rmdir "$dir/$base"
         fi
+        
+        # Move the entire resulting module folder to the web root
         mv "$dir" /var/www/html/modules/
     fi
 done
