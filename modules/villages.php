2596abc71e03005c42a9e72383653f431300c55a
jaoe1000 <jaoe1000@aol.com>
2026-06-14 23:51:55 -0400

    Update villages.php

--- modules/villages.php
+++ modules/villages.php
@@ -343,6 +343,9 @@ function villages_run()
                     "$continueTravelUri&"
                 );
 				if ($travelEvent != 0) {
+					if (is_array($vtext['title'])) {
+    					die("<pre>CRASH INTERCEPTED. Title contains: " . print_r($vtext['title'], true) . "</pre>");
+					}
 					page_header('villages.headers.event');
 					if (checknavs()) {
 						page_footer();
