83aa11e2738c9958a06a85d8e2dbf4c89047f107
jaoe1000 <jaoe1000@aol.com>
2026-06-18 17:58:28 -0400

    Convert Naruto ninjutsu specialties to standalone specialty modules matching the master blueprint

---
+++ modules/specialtygenjutsu.php
@@ -0,0 +1,240 @@
+<?php
+/**
+ * Advanced Specialty Module - Genjutsu
+ * Built from the Advanced Specialty Blueprint
+ * Designed for PHP 8.4 Strict Compliance
+ */
+
+function specialtygenjutsu_getmoduleinfo() {
+    return [
+        "name" => "Specialty - Genjutsu",
+        "version" => "1.0",
+        "author" => "Dragon.NDGaming", 
+        "category" => "Specialties",
+        "download" => "", 
+        "settings" => [
+            "Genjutsu - Settings,title",
+            "dklimit" => "Limit the Specialty to a certain amount of dragon kills?,int|0",
+            "wepchange" => "Will weapons & armour change names?,bool|1", 
+            "IE- an 'Adze' would become a 'Genjutsu Adze',note", 
+        ],
+        "prefs" => [
+            "Genjutsu - User Prefs,title",
+            "skill" => "Skill points in Specialty Powers,int|0", 
+            "uses" => "Uses of Specialty Powers allowed,int|0", 
+            "class" => "Main Class,enum,Unset,,Magic,magic,Melee,melee,Ranging,ranging|magic", 
+            "subclass" => "Subclass,enum,Unset,0,Default,1|1", 
+        ]
+    ];
+}
+
+function specialtygenjutsu_install() {
+    module_addhook("choose-specialty");
+    module_addhook("set-specialty");
+    module_addhook("fightnav-specialties");
+    module_addhook("apply-specialties");
+    module_addhook("newday");
+    module_addhook("incrementspecialty");
+    module_addhook("specialtynames");
+    module_addhook("specialtymodules");
+    module_addhook("specialtycolor");
+    module_addhook("dragonkill");
+    module_addhook("newday-intercept");
+    module_addhook("boughtweapon");
+    module_addhook("boughtarmor");
+    debug("Installed 'Genjutsu' specialty module.");
+    return true;
+}
+
+function specialtygenjutsu_uninstall() {
+    $spec = "GJ"; 
+    global $session;
+    
+    // Reset the specialty for any user currently possessing it
+    $sql = "UPDATE " . db_prefix("accounts") . " SET specialty='' WHERE specialty='".$spec."'";
+    db_query($sql);
+    
+    if (($session['user']['specialty'] ?? '') === $spec) {
+        $session['user']['specialty'] = "";
+    }
+    
+    debug("Uninstalled 'Genjutsu' specialty module.");
+    return true;
+}
+
+function specialtygenjutsu_dohook($hookname, $args) {
+    global $session, $resline;
+    $spec = "GJ"; 
+    $name = "Genjutsu"; 
+    $ccode = "`%"; 
+    
+    $userSpec = $session['user']['specialty'] ?? '';
+    
+    $techniques = [
+
+    ];
+    
+    switch($hookname) {
+        case "newday-intercept":
+            specialtygenjutsu_change();
+            break;
+
+        case "newday":
+            if ($userSpec === $spec) {
+                $bonus = (int)getsetting("specialtybonus", 1);
+                
+                if ($bonus == 1) {
+                    output("`n`2For your specialty in %s%s`2, you receive `^1`2 extra `&%s%s`2 use for today.`n", $ccode, $name, $ccode, $name);
+                } else {
+                    output("`n`2For your specialty in %s%s`2, you receive `^%s`2 extra `&%s%s`2 uses for today.`n", $ccode, $name, $bonus, $ccode, $name);
+                }
+                
+                $amt = (int)(get_module_pref("skill") / 3);
+                set_module_pref("uses", $amt + $bonus);
+            }
+            break;
+
+        case "dragonkill":
+            set_module_pref("uses", 0);
+            set_module_pref("skill", 0);
+            break;
+
+        case "choose-specialty":
+            if ($userSpec === "" || $userSpec === '0') {
+                $can_select = true;
+
+                if (($session['user']['dragonkills'] ?? 0) < (int)get_module_setting("dklimit")) {
+                    $can_select = false;
+                }
+
+                if ($can_select) {
+                    addnav("$ccode$name`0", "newday.php?setspecialty=".$spec."$resline");
+                    $t1 = translate_inline("Train in Genjutsu"); 
+                    $t2 = appoencode(translate_inline("$ccode$name`0"));
+                    rawoutput("<a href='newday.php?setspecialty=$spec$resline'>$t1 ($t2)</a><br>");
+                    addnav("", "newday.php?setspecialty=$spec$resline");
+                }
+            }
+            break;
+
+        case "set-specialty":
+            if ($userSpec === $spec) {
+                page_header($name);
+                output("`#`c`bGenjutsu Unlocked`b`c`n");
+                output("`@You have unlocked the secrets of Genjutsu! Focus your chakra and unleash your Jutsu in combat.`n");
+            }
+            break;
+
+        case "fightnav-specialties":
+            if ($userSpec === $spec) {
+                $uses = (int)get_module_pref("uses");
+                $script = $args['script'] ?? '';
+                
+                if ($uses > 0) {
+                    addnav(["$ccode $name (%s points)`0", $uses], "");
+                }
+                
+
+            }
+            break;
+
+        case "apply-specialties":
+            $skill = httpget('skill');
+            $l = (int)httpget('l');
+            
+            if ($skill === $spec) {
+                $cost = $techniques[$l]['cost'] ?? 0;
+                if ((int)get_module_pref("uses") >= $cost) {
+                    $code = $techniques[$l]['code'] ?? '';
+                    switch ($code) {
+case "genjutsu1":
+			apply_buff('genjutsu1',array(
+				"startmsg"=>"`%`iNarakumi no Jutsu`i`n`vAn imaginary circle of leaves spin around and envelop the enemy, falling away shortly after.",
+				"name"=>"`%Narakumi no Jutsu",
+				"rounds"=>5,
+				"wearoff"=>"{badgu
+                    }
+                    set_module_pref("uses", (int)get_module_pref("uses") - $cost);
+                }
+            }
+            break;
+
+        case "incrementspecialty":
+            if($userSpec === $spec) {
+                $new = (int)get_module_pref("skill") + 1;
+                set_module_pref("skill", $new);
+                $c = $args['color'] ?? '';
+                
+                output("`n%sYou gain a level in `&%s%s to `#%s%s!", $c, $name, $c, $new, $c);
+                $x = $new % 3;
+                
+                if ($x == 0) {
+                    output("`n`^You gain an extra use point!`n");
+                    set_module_pref("uses", (int)get_module_pref("uses") + 1);
+                } else {
+                    if (3-$x == 1) {
+                        output("`n`^Only 1 more skill level until you gain an extra use point!`n");
+                    } else {
+                        output("`n`^Only %s more skill levels until you gain an extra use point!`n", (3-$x));
+                    }
+                }
+                output_notl("`0");
+            }
+            break;
+
+        case "specialtynames":
+            if (($session['user']['dragonkills'] ?? 0) >= (int)get_module_setting("dklimit")) {
+                $args[$spec] = translate_inline($name);
+            }
+            break;
+
+        case "specialtymodules":
+            $args[$spec] = "specialtygenjutsu";
+            break;
+
+        case "specialtycolor":
+            $args[$spec] = $ccode;
+            break;
+            
+        case "boughtweapon":
+        case "boughtarmor":
+            if (get_module_setting("wepchange") == 1 && $userSpec === $spec) {
+                $nstr = "`%" . specialtygenjutsu_namer("Y");
+                
+                if (!preg_match('/' . preg_quote($nstr, '/') . '/', $args['name'] ?? '')) {
+                    $args['name'] = $nstr . " " . $args['name'];
+                }
+            }
+            break;
+    }
+    return $args;
+}
+
+function specialtygenjutsu_run() {
+}
+
+function specialtygenjutsu_change() {
+    global $session;
+    $spec = "GJ"; 
+    
+    if (get_module_setting("wepchange") == 1 && ($session['user']['specialty'] ?? '') === $spec) {
+        $nstr = "`%".specialtygenjutsu_namer("Y");
+        
+        if (!preg_match('/' . preg_quote($nstr, '/') . '/', $session['user']['weapon'] ?? '')) {
+            $n = $nstr." ".($session['user']['weapon'] ?? '');
+            if (strlen($n) < 50) $session['user']['weapon'] = $n;
+        }
+        
+        if (!preg_match('/' . preg_quote($nstr, '/') . '/', $session['user']['armor'] ?? '')) {
+            $n = $nstr." ".($session['user']['armor'] ?? '');
+            if (strlen($n) < 50) $session['user']['armor'] = $n;
+        }
+    }
+}
+
+function specialtygenjutsu_namer($type='X') {
+    $class = [];
+    $class['1'] = "Genjutsu";
+    return $class['1'];
+}
+?>
---
+++ modules/specialtyice.php
@@ -0,0 +1,237 @@
+<?php
+/**
+ * Advanced Specialty Module - Hyouton Ninjutsu
+ * Built from the Advanced Specialty Blueprint
+ * Designed for PHP 8.4 Strict Compliance
+ */
+
+function specialtyice_getmoduleinfo() {
+    return [
+        "name" => "Specialty - Hyouton Ninjutsu",
+        "version" => "1.0",
+        "author" => "Dragon.NDGaming", 
+        "category" => "Specialties",
+        "download" => "", 
+        "settings" => [
+            "Hyouton Ninjutsu - Settings,title",
+            "dklimit" => "Limit the Specialty to a certain amount of dragon kills?,int|0",
+            "wepchange" => "Will weapons & armour change names?,bool|1", 
+            "IE- an 'Adze' would become a 'Hyouton Ninjutsu Adze',note", 
+        ],
+        "prefs" => [
+            "Hyouton Ninjutsu - User Prefs,title",
+            "skill" => "Skill points in Specialty Powers,int|0", 
+            "uses" => "Uses of Specialty Powers allowed,int|0", 
+            "class" => "Main Class,enum,Unset,,Magic,magic,Melee,melee,Ranging,ranging|magic", 
+            "subclass" => "Subclass,enum,Unset,0,Default,1|1", 
+        ]
+    ];
+}
+
+function specialtyice_install() {
+    module_addhook("choose-specialty");
+    module_addhook("set-specialty");
+    module_addhook("fightnav-specialties");
+    module_addhook("apply-specialties");
+    module_addhook("newday");
+    module_addhook("incrementspecialty");
+    module_addhook("specialtynames");
+    module_addhook("specialtymodules");
+    module_addhook("specialtycolor");
+    module_addhook("dragonkill");
+    module_addhook("newday-intercept");
+    module_addhook("boughtweapon");
+    module_addhook("boughtarmor");
+    debug("Installed 'Hyouton Ninjutsu' specialty module.");
+    return true;
+}
+
+function specialtyice_uninstall() {
+    $spec = "IC"; 
+    global $session;
+    
+    // Reset the specialty for any user currently possessing it
+    $sql = "UPDATE " . db_prefix("accounts") . " SET specialty='' WHERE specialty='".$spec."'";
+    db_query($sql);
+    
+    if (($session['user']['specialty'] ?? '') === $spec) {
+        $session['user']['specialty'] = "";
+    }
+    
+    debug("Uninstalled 'Hyouton Ninjutsu' specialty module.");
+    return true;
+}
+
+function specialtyice_dohook($hookname, $args) {
+    global $session, $resline;
+    $spec = "IC"; 
+    $name = "Hyouton Ninjutsu"; 
+    $ccode = "`l"; 
+    
+    $userSpec = $session['user']['specialty'] ?? '';
+    
+    $techniques = [
+
+    ];
+    
+    switch($hookname) {
+        case "newday-intercept":
+            specialtyice_change();
+            break;
+
+        case "newday":
+            if ($userSpec === $spec) {
+                $bonus = (int)getsetting("specialtybonus", 1);
+                
+                if ($bonus == 1) {
+                    output("`n`2For your specialty in %s%s`2, you receive `^1`2 extra `&%s%s`2 use for today.`n", $ccode, $name, $ccode, $name);
+                } else {
+                    output("`n`2For your specialty in %s%s`2, you receive `^%s`2 extra `&%s%s`2 uses for today.`n", $ccode, $name, $bonus, $ccode, $name);
+                }
+                
+                $amt = (int)(get_module_pref("skill") / 3);
+                set_module_pref("uses", $amt + $bonus);
+            }
+            break;
+
+        case "dragonkill":
+            set_module_pref("uses", 0);
+            set_module_pref("skill", 0);
+            break;
+
+        case "choose-specialty":
+            if ($userSpec === "" || $userSpec === '0') {
+                $can_select = true;
+
+                if (($session['user']['dragonkills'] ?? 0) < (int)get_module_setting("dklimit")) {
+                    $can_select = false;
+                }
+
+                if ($can_select) {
+                    addnav("$ccode$name`0", "newday.php?setspecialty=".$spec."$resline");
+                    $t1 = translate_inline("Train in Hyouton Ninjutsu"); 
+                    $t2 = appoencode(translate_inline("$ccode$name`0"));
+                    rawoutput("<a href='newday.php?setspecialty=$spec$resline'>$t1 ($t2)</a><br>");
+                    addnav("", "newday.php?setspecialty=$spec$resline");
+                }
+            }
+            break;
+
+        case "set-specialty":
+            if ($userSpec === $spec) {
+                page_header($name);
+                output("`#`c`bHyouton Ninjutsu Unlocked`b`c`n");
+                output("`@You have unlocked the secrets of Hyouton Ninjutsu! Focus your chakra and unleash your Jutsu in combat.`n");
+            }
+            break;
+
+        case "fightnav-specialties":
+            if ($userSpec === $spec) {
+                $uses = (int)get_module_pref("uses");
+                $script = $args['script'] ?? '';
+                
+                if ($uses > 0) {
+                    addnav(["$ccode $name (%s points)`0", $uses], "");
+                }
+                
+
+            }
+            break;
+
+        case "apply-specialties":
+            $skill = httpget('skill');
+            $l = (int)httpget('l');
+            
+            if ($skill === $spec) {
+                $cost = $techniques[$l]['cost'] ?? 0;
+                if ((int)get_module_pref("uses") >= $cost) {
+                    $code = $techniques[$l]['code'] ?? '';
+                    switch ($code) {
+case "ice1":
+			apply_buff('ice1',array(
+				"startmsg"=>"`l`i`1Hyou`vton `q-`v Hyourou no Jutsu!`i`n`1You `v trap {badgu
+                    }
+                    set_module_pref("uses", (int)get_module_pref("uses") - $cost);
+                }
+            }
+            break;
+
+        case "incrementspecialty":
+            if($userSpec === $spec) {
+                $new = (int)get_module_pref("skill") + 1;
+                set_module_pref("skill", $new);
+                $c = $args['color'] ?? '';
+                
+                output("`n%sYou gain a level in `&%s%s to `#%s%s!", $c, $name, $c, $new, $c);
+                $x = $new % 3;
+                
+                if ($x == 0) {
+                    output("`n`^You gain an extra use point!`n");
+                    set_module_pref("uses", (int)get_module_pref("uses") + 1);
+                } else {
+                    if (3-$x == 1) {
+                        output("`n`^Only 1 more skill level until you gain an extra use point!`n");
+                    } else {
+                        output("`n`^Only %s more skill levels until you gain an extra use point!`n", (3-$x));
+                    }
+                }
+                output_notl("`0");
+            }
+            break;
+
+        case "specialtynames":
+            if (($session['user']['dragonkills'] ?? 0) >= (int)get_module_setting("dklimit")) {
+                $args[$spec] = translate_inline($name);
+            }
+            break;
+
+        case "specialtymodules":
+            $args[$spec] = "specialtyice";
+            break;
+
+        case "specialtycolor":
+            $args[$spec] = $ccode;
+            break;
+            
+        case "boughtweapon":
+        case "boughtarmor":
+            if (get_module_setting("wepchange") == 1 && $userSpec === $spec) {
+                $nstr = "`%" . specialtyice_namer("Y");
+                
+                if (!preg_match('/' . preg_quote($nstr, '/') . '/', $args['name'] ?? '')) {
+                    $args['name'] = $nstr . " " . $args['name'];
+                }
+            }
+            break;
+    }
+    return $args;
+}
+
+function specialtyice_run() {
+}
+
+function specialtyice_change() {
+    global $session;
+    $spec = "IC"; 
+    
+    if (get_module_setting("wepchange") == 1 && ($session['user']['specialty'] ?? '') === $spec) {
+        $nstr = "`%".specialtyice_namer("Y");
+        
+        if (!preg_match('/' . preg_quote($nstr, '/') . '/', $session['user']['weapon'] ?? '')) {
+            $n = $nstr." ".($session['user']['weapon'] ?? '');
+            if (strlen($n) < 50) $session['user']['weapon'] = $n;
+        }
+        
+        if (!preg_match('/' . preg_quote($nstr, '/') . '/', $session['user']['armor'] ?? '')) {
+            $n = $nstr." ".($session['user']['armor'] ?? '');
+            if (strlen($n) < 50) $session['user']['armor'] = $n;
+        }
+    }
+}
+
+function specialtyice_namer($type='X') {
+    $class = [];
+    $class['1'] = "Hyouton Ninjutsu";
+    return $class['1'];
+}
+?>
